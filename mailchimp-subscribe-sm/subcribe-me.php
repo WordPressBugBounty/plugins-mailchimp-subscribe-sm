84a30814732f8b89d627881224361229d4ac692f
Sean Murphy <sean@iamseanmurphy.com>
2025-06-21 21:17:08 +0000

    4.3.2

--- mailchimp-subscribe-sm/subcribe-me.php
+++ mailchimp-subscribe-sm/subcribe-me.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: PluginOps Optin Builder
-Description: The easy to use tool for creating highly attractive optin popups, bars and email capture forms on your website to increase converions.
+Description: The easy to use tool for creating highly attractive optin popups, bars and email capture forms on your website to increase conversions.
 Author: PluginOps
 Plugin URI: https://pluginops.com/optin-builder/
 Author URI: https://pluginops.com/optin-builder/
-Version: 4.3.1
+Version: 4.3.2
 Donate link: https://pluginops.com/optin-builder/
 License: GPL V2
 */
